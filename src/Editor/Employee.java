7a1aed3e5491378c80762be87a2fbed412368992
huelsma2 <huelsma2@ceas-942-mac13.cae.uwm.edu>
2018-03-06 11:39:25 -0600

    emp

--- src/Editor/Employee.java
+++ src/Editor/Employee.java
@@ -2,4 +2,36 @@ package Editor;
 
 public class Employee {
 
+	private String _fname, _lname, _phone, _department;
+	
+	public Employee(String fname, String lname, String phone, String dept)
+	{
+		this._fname = fname;
+		this._lname = lname;
+		this._phone = phone;
+		this._department = dept;
+	}
+
+	public void set_fname(String _fname) {
+		this._fname = _fname;
+	}
+
+	public void set_lname(String _lname) {
+		this._lname = _lname;
+	}
+
+	public void set_phone(String _phone) {
+		this._phone = _phone;
+	}
+
+	public void set_department(String _department) {
+		this._department = _department;
+	}
+	
+	
+	@Override
+	public String toString()
+	{
+		return _lname + ", " + _fname + " " + _phone + " " + _department;
+	}
 }
