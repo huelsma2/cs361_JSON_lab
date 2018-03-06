e73bf21ba265e330b3ec1d4b9e78fcd30cd73bae
andrew <andrew@10.22.202.162>
2018-03-06 12:07:30 -0600

    Began MainDirectory Class

--- src/Directory_Server/MainDirectory.java
+++ src/Directory_Server/MainDirectory.java
@@ -1,5 +1,35 @@
 package Directory_Server;
 
-public class MainDirectory {
+import java.util.ArrayList;
+import java.util.Collection;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import Editor.Employee;
+import Editor.IDirectory;
+
+
+public class MainDirectory  implements IDirectory{
+	
+	ArrayList<Employee> dir = new ArrayList<Employee>();
+	
+	public void add(String json){
+		
+		ArrayList<Employee> list = new Gson().fromJson(json, new TypeToken<Collection<Employee>>(){}.getType());
+		dir.addAll(list);
+		
+	}
+	
+	public void print(){
+		
+		
+		
+	}
+	
+	public void clear(){
+		
+	}
+	
+	
 }
